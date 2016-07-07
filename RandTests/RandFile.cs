205db27b675e0eb39c1b8f71300a46729ec85945
Jacob Dawson <b.jacob.dawson@gmail.com>
2016-07-07 22:03:30 +0200

    Updated RNGs

---
+++ RandTests/RandFile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+
+namespace Vulpine.Core.Data.RandGen
+{
+    public class RandFile : VRandom, IDisposable
+    {
+        private BinaryReader br;
+
+        public RandFile(string file)
+        {
+            Stream s = File.Open(file, FileMode.Open, FileAccess.Read);
+            br = new BinaryReader(s);
+            seed = 0;
+        }
+
+        public override int NextInt()
+        {
+            return br.ReadInt32();
+        }
+
+        public override double NextDouble()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            br.Dispose();
+        }
+    }
+}
